2c8dbc212aa96383e75cc1ce88af39c3993dfc78
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-02 22:25:13 +0530

    Add Even or Odd Checker implementation with user input and conditional statements

--- java/03-even-odd.java
+++
@@ -1,9 +0,0 @@
-/*
-3. Even or Odd Checker
-• Objective: Utilize conditional statements.
-• Task: Determine if a number entered by the user is even or odd.
-• Instructions:
-o Prompt the user for an integer.
-o Use the modulus operator % to check divisibility by 2.
-o Display whether the number is even or odd.
-*/
