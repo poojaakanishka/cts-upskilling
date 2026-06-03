64dc2e65587d41958ba3c8277af26f2bf6418280
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 20:38:49 +0530

    Add SQL query to list events without uploaded resources

--- sql/18-resource-check.sql
+++ sql/18-resource-check.sql
@@ -2,3 +2,12 @@
 18. Resource Availability Check
 List all events that do not have any resources uploaded.
 */
+
+
+SELECT
+    e.event_id,
+    e.event_name
+FROM Events e
+LEFT JOIN Resources r
+    ON e.event_id = r.event_id
+WHERE r.resource_id IS NULL;
