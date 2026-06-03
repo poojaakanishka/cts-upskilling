3b83ac5bb6fe8a38de263640a9634dd2360b81f1
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 20:23:05 +0530

    Add SQL query to list the top 5 cities with the highest number of distinct user registrations

--- sql/05-active-cities.sql
+++ sql/05-active-cities.sql
@@ -2,3 +2,14 @@
 5. Most Active Cities
 List the top 5 cities with the highest number of distinct user registrations.
 */
+
+
+SELECT
+    u.city,
+    COUNT(DISTINCT r.user_id) AS total_registrations
+FROM Users u
+JOIN Registrations r
+    ON u.user_id = r.user_id
+GROUP BY u.city
+ORDER BY total_registrations DESC
+LIMIT 5;
