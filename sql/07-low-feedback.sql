496b327f331b849b70dd1c605372d9671e80c657
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 20:26:02 +0530

    Add SQL query to list users with feedback ratings less than 3

--- sql/07-low-feedback.sql
+++ sql/07-low-feedback.sql
@@ -3,3 +3,16 @@
 List all users who gave feedback with a rating less than 3, along with their comments and
 associated event names.
 */
+
+SELECT
+    u.user_id,
+    u.user_name,
+    e.event_name,
+    f.rating,
+    f.comment
+FROM Feedback f
+JOIN Users u
+    ON f.user_id = u.user_id
+JOIN Events e
+    ON f.event_id = e.event_id
+WHERE f.rating < 3;
