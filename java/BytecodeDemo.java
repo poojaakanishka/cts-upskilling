3260bd7441cb88138fde59e75ec379a2d45b4284
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 14:47:02 +0530

    Add BytecodeDemo example to demonstrate using javap for bytecode inspection

--- java/BytecodeDemo.java
+++ java/BytecodeDemo.java
@@ -7,3 +7,14 @@ o Create a class with a method.
 o Compile it and run javap -c ClassName.
 o Interpret the bytecode output.
 */
+public class BytecodeDemo {
+
+    public void greet() {
+        System.out.println("Hello from Bytecode!");
+    }
+
+    public static void main(String[] args) {
+        BytecodeDemo obj = new BytecodeDemo();
+        obj.greet();
+    }
+}
