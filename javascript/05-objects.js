a56202f194b36f336df68a3bc26bfc5f69988838
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 19:30:00 +0530

    Implement Event class with availability check and details logging

--- javascript/05-objects.js
+++ javascript/05-objects.js
@@ -7,3 +7,28 @@ Task:
 • Add checkAvailability() to prototype
 • List object keys and values using Object.entries()
 */
+function Event(name, category, seats) {
+    this.name = name;
+    this.category = category;
+    this.seats = seats;
+}
+
+Event.prototype.checkAvailability = function () {
+    if (this.seats > 0) {
+        console.log(`${this.name} is available. Seats left: ${this.seats}`);
+    } else {
+        console.log(`${this.name} is full.`);
+    }
+};
+
+const event1 = new Event("Music Night", "Music", 50);
+const event2 = new Event("Tech Talk", "Technology", 0);
+
+event1.checkAvailability();
+event2.checkAvailability();
+
+console.log("\nEvent Details:");
+
+Object.entries(event1).forEach(([key, value]) => {
+    console.log(`${key}: ${value}`);
+});
