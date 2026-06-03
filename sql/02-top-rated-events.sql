df03ddd694ebc553f6042116412d1227c5bef356
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 20:16:52 +0530

    Add SQL query to retrieve top rated events based on average ratings and feedback count

--- sql/02-top-rated-events.sql
+++ sql/02-top-rated-events.sql
@@ -3,3 +3,15 @@
 Identify events with the highest average rating, considering only those that have received at
 least 10 feedback submissions.
 */
+
+SELECT
+    e.event_id,
+    e.event_name,
+    AVG(f.rating) AS average_rating,
+    COUNT(f.rating) AS total_feedbacks
+FROM Events e
+JOIN Feedback f
+    ON e.event_id = f.event_id
+GROUP BY e.event_id, e.event_name
+HAVING COUNT(f.rating) >= 10
+ORDER BY average_rating DESC;
