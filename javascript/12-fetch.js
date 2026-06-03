ef88805517c5c229fa4e77f0565c189ed137492e
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 19:38:42 +0530

    Implement user registration functionality with fetch API and simulated response

--- javascript/12-fetch.js
+++ javascript/12-fetch.js
@@ -7,3 +7,30 @@ Task:
 • Show success/failure message after submission
 • Use setTimeout() to simulate a delayed response
 */
+
+const userData = {
+    name: "Kani",
+    email: "kani@example.com",
+    event: "Music Night"
+};
+
+setTimeout(() => {
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Registration Successful!");
+        console.log(data);
+    })
+    .catch(error => {
+        console.log("Registration Failed!");
+        console.error(error);
+    });
+
+}, 2000); 
