579cc676e7d59e5b648a281183978e1ff6555d78
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 20:22:36 +0530

    Add SQL query to count sessions scheduled between 10 AM and 12 PM for each event

--- sql/04-peak-hours.sql
+++ sql/04-peak-hours.sql
@@ -2,3 +2,11 @@
 4. Peak Session Hours
 Count how many sessions are scheduled between 10 AM to 12 PM for each event.
 */
+
+
+SELECT
+    event_id,
+    COUNT(session_id) AS session_count
+FROM Sessions
+WHERE session_time BETWEEN '10:00:00' AND '12:00:00'
+GROUP BY event_id;
