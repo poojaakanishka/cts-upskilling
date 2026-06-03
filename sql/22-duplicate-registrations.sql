8619466f3b5cae606734c10994951f06b5fdb11a
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 20:45:35 +0530

    Add SQL query to check for duplicate registrations for the same event

--- sql/22-duplicate-registrations.sql
+++ sql/22-duplicate-registrations.sql
@@ -2,3 +2,13 @@
 22. Duplicate Registrations Check
 Detect if a user has been registered more than once for the same event.
 */
+
+SELECT
+    user_id,
+    event_id,
+    COUNT(*) AS registration_count
+FROM Registrations
+GROUP BY
+    user_id,
+    event_id
+HAVING COUNT(*) > 1;
