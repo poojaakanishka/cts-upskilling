3de4655af80b90a11c1003dcf306ae906109f1d4
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 20:43:37 +0530

    Add SQL query to list top 5 users with the most feedback entries

--- sql/21-top-feedback.sql
+++ sql/21-top-feedback.sql
@@ -2,3 +2,17 @@
 21. Top Feedback Providers
 List top 5 users who have submitted the most feedback entries.
 */
+
+
+SELECT
+    u.user_id,
+    u.user_name,
+    COUNT(f.feedback_id) AS total_feedbacks
+FROM Users u
+JOIN Feedback f
+    ON u.user_id = f.user_id
+GROUP BY
+    u.user_id,
+    u.user_name
+ORDER BY total_feedbacks DESC
+LIMIT 5;
