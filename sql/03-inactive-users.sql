440a396f5f402f0ec0eccbfe24c87bd2855367be
poojaakanishka <tpoojaa2006@gmail.com>
2026-06-03 20:22:00 +0530

    Add SQL query to retrieve inactive users who have not registered for events in the last 90 days

--- sql/03-inactive-users.sql
+++ sql/03-inactive-users.sql
@@ -2,3 +2,13 @@
 3. Inactive Users
 Retrieve users who have not registered for any events in the last 90 days.
 */
+
+SELECT
+    u.user_id,
+    u.user_name
+FROM Users u
+WHERE u.user_id NOT IN (
+    SELECT r.user_id
+    FROM Registrations r
+    WHERE r.registration_date >= CURRENT_DATE - INTERVAL 90 DAY
+);
